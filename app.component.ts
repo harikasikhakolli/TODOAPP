007350ccad518cc2cd39ba9cb0315e5e9188f0e2
harikasikhakolli <39305218+harikasikhakolli@users.noreply.github.com>
2020-08-08 19:26:21 +0530

    Add files via upload

---
+++ app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms'
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  public items = []; 
+  public newTask; 
+  public addToList() { 
+      if (this.newTask == '') { 
+      } 
+      else { 
+          this.items.push(this.newTask); 
+          this.newTask = ''; 
+      } 
+  } 
+  public deleteTask(index) { 
+      this.items.splice(index, 1); 
+  } 
+}
